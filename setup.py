c41bcc20542c88210b48a1a12c924b9940061a06
Alan D. Snow <alansnow21@gmail.com>
2022-04-04 16:24:50 -0500

    REF: use setup.cfg (#99)

--- setup.py
+++ setup.py
@@ -1,70 +1,7 @@
 """The setup script."""
 
-from itertools import chain
-
-from setuptools import find_packages, setup
+from setuptools import setup
 
 from geocube import __version__
 
-with open("README.rst") as readme_file:
-    readme = readme_file.read()
-
-requirements = [
-    "appdirs",
-    "Click>=6.0",
-    "geopandas>=0.7",
-    "odc_geo",
-    "rasterio",
-    "rioxarray>=0.4",
-    "scipy",
-    "xarray>=0.17",
-    "pyproj>=2",
-]
-
-test_requirements = ["pytest>=3.6", "pytest-cov", "packaging", "netcdf4"]
-doc_requirements = ["sphinx-click", "nbsphinx", "sphinx_rtd_theme"]
-
-extras_require = {
-    "doc": doc_requirements,
-    "dev": test_requirements
-    + doc_requirements
-    + [
-        "black",
-        "flake8",
-        "pylint",
-        "isort",
-        "pre-commit",
-    ],
-}
-extras_require["all"] = list(chain.from_iterable(extras_require.values()))
-
-setup(
-    author="GeoCube Contributors",
-    author_email="alansnow21@gmail.com",
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-    ],
-    description="Tool to convert geopandas vector data into rasterized xarray data.",
-    entry_points={"console_scripts": ["geocube=geocube.cli.geocube:geocube"]},
-    install_requires=requirements,
-    license="BSD license",
-    long_description=readme + "\n\n",
-    include_package_data=True,
-    keywords="geocube",
-    name="geocube",
-    packages=find_packages(include=["geocube*"]),
-    test_suite="test",
-    tests_require=test_requirements,
-    extras_require=extras_require,
-    url="https://github.com/corteva/geocube",
-    version=__version__,
-    zip_safe=False,
-    python_requires=">=3.8",
-)
+setup(version=__version__)
